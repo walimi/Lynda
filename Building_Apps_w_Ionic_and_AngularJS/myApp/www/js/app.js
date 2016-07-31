197de26d524adda2c2c5ed889bfe1df14de32da2
Wahid Alimi <wahid.alimi@gmail.com>
2016-07-31 08:05:39 -0400

    Building a Single View App: Using Angular Controllers

--- Building_Apps_w_Ionic_and_AngularJS/myApp/www/js/app.js
+++ Building_Apps_w_Ionic_and_AngularJS/myApp/www/js/app.js
@@ -22,3 +22,10 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.controller('ListController', ['$scope','$http', function($scope, $http){
+  $http.get('js/data.json')
+      .success(function(data) {
+        $scope.artists = data;
+      });
+}]);
